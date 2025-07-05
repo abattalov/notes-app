7e2741b912761445a1729dff3bf22ec1617ac27f
Alisher Battalov <71417582+abattalov@users.noreply.github.com>
2025-07-05 11:06:03 -0500

    added some styling and starting to implement logic on selecting specific notes to render

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,12 +1,18 @@
 import type { Note } from "./../types";
+import "./Notecard.css"
 
 type NoteCardProps = {
     note: Note;
 }
 
 export default function NoteCard(props: NoteCardProps){
+
+    const openNote = () => {
+        console.log(props.note.id)
+    }
+
     return (
-        <div>
+        <div className="notecard-cont" onClick={openNote}>
             <h4>{props.note.title}</h4>
             <h5>{props.note.updated_at}</h5>
         </div>
