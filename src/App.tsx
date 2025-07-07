d9a9612f98570559fa7d95cee9581626f49be79f
Alisher Battalov <71417582+abattalov@users.noreply.github.com>
2025-07-07 08:38:08 -0500

    added create note functionality, moved note fetching and state up to parent component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,39 @@ import './App.css'
 import './components/SidePanel.css'
 import SidePanel from './components/SidePanel'
 import TextArea from './components/TextArea'
-import { useState, useCallback, useRef } from 'react'
+import type { Note } from './types'
+import { useState, useCallback, useRef, useEffect } from 'react'
 
 function App() {
 
   const [sidePanelOpen, setSidePanelOpen] = useState<boolean>(true)
   const [noteContent, setNoteContent] = useState<string>("")
   const [currentNoteId, setCurrentNoteId] = useState<number | null>(null)
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   const saveTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    fetchNotes();
+  }, [])
+
+  const fetchNotes = async () => {
+    try{
+
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/notes`);
+        const data = await response.json();
+        setNotes(data);
+
+    } catch(error){
+
+        setError(error instanceof Error ? error.message : "Unknown error")
+
+    } finally{
+        setLoading(false);
+    }
+  }
 
   const toggleSidePanel = () => {
     setSidePanelOpen(!sidePanelOpen)
@@ -73,11 +96,40 @@ function App() {
     }
   }
 
+  const createNewNote = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/notes/`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          title: "New Note",
+          content: '',
+          tags: []
+        })
+      })
+
+      if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const newNote = await response.json();
+
+      await fetchNotes();
+
+      openNote(newNote.id)
+
+    } catch(error){
+      setError(error instanceof Error ? error.message : "Uknown error")
+    }
+  }
+
   return (
     <div className='main-cont'>
       <SidePanel 
+        notes={notes}
         isOpen={sidePanelOpen} 
-        openNote={openNote}/>
+        openNote={openNote}
+        createNewNote={createNewNote}/>
       <TextArea 
         isFullscreen={!sidePanelOpen} 
         toggleSidePanel={toggleSidePanel} 
--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -2,47 +2,27 @@ import NotesList from "./NoteList"
 import Searchbar from "./Searchbar"
 import "./SidePanel.css"
 import type { Note } from './../types';
-import { useEffect, useState } from "react"
 
 export type SidePanelProps = {
+    notes: Note[];
     isOpen: boolean;
+    error: string | null;
+    loading: boolean;
     openNote: (id: number) => void;
+    createNewNote: () => void;
 }
 
 export default function SidePanel(props: SidePanelProps){
 
-    const [notes, setNotes] = useState<Note[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        const fetchNotes = async () => {
-            try{
-
-                const response = await fetch(`${import.meta.env.VITE_API_URL}/api/notes`);
-                const data = await response.json();
-                setNotes(data);
-
-            } catch(error){
-
-                setError(error instanceof Error ? error.message : "Unknown error")
-
-            } finally{
-                setLoading(false);
-            }
-        }
-
-        fetchNotes();
-    }, [])
-
-    if(loading) return <div>Loading...</div>
-    if(error) return <div>Error: {error}</div>
+    if(props.loading) return <div>Loading...</div>
+    if(props.error) return <div>Error: {props.error}</div>
 
     return (
             <div className={props.isOpen ? "sidepanel-open" : "sidepanel"}>
                 <h1>My Notes</h1>
                 <Searchbar/>
-                <NotesList notes={notes} openNote={props.openNote}/>
+                <button onClick={props.createNewNote}>+</button>
+                <NotesList notes={props.notes} openNote={props.openNote}/>
             </div>
     )
 }
